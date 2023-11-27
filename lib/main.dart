ec424c1783595be2d447c4d3d90e526dbe4d4328
adityaaugusta <aditya.augusta@jojonomic.com>
2023-11-27 22:55:04 +0700

    add stetho

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:kaimovies/core/app.dart';
 import 'package:kaimovies/utilities/injector.dart';
+import 'package:stetho_network_inspector/stetho_network_inspector.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Stetho.initialize();
 
   await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
