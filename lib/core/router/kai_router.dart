5dd6bce8b105e541f82fc31bb8c78333f81afca4
adityaaugusta <aditya.augusta@jojonomic.com>
2023-11-29 13:55:12 +0700

    refactor

--- lib/core/router/kai_router.dart
+++ lib/core/router/kai_router.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kaimovies/core/home/screens/pages/home_page.dart';
+import 'package:kaimovies/features/detail/screens/blocs/movie_detail_cubit.dart';
 import 'package:kaimovies/features/movie/screens/pages/movie_detail_page.dart';
 import 'package:kaimovies/features/tv/screens/pages/tv_detail_page.dart';
 
@@ -24,11 +26,7 @@ class HomeRoute extends GoRouteData {
   const HomeRoute();
 
   @override
-  Widget build(
-    BuildContext context,
-    GoRouterState state,
-  ) =>
-      const HomePage();
+  Widget build(BuildContext context, GoRouterState state) => const HomePage();
 }
 
 @immutable
@@ -38,11 +36,9 @@ class MovieDetailRoute extends GoRouteData {
   final String id;
 
   @override
-  Widget build(
-    BuildContext context,
-    GoRouterState state,
-  ) =>
-      MovieDetailPage(movieId: id);
+  Widget build(BuildContext context, GoRouterState state) => BlocProvider(
+      create: (context) => MovieDetailCubit.create(context)..fetch(id),
+      child: MovieDetailPage(movieId: id));
 }
 
 @immutable
@@ -52,9 +48,6 @@ class TvDetailRoute extends GoRouteData {
   final String id;
 
   @override
-  Widget build(
-    BuildContext context,
-    GoRouterState state,
-  ) =>
+  Widget build(BuildContext context, GoRouterState state) =>
       TvDetailPage(tvId: id);
 }
